07a142c81e3e43053b119b11356cff86e0ce67d7
Mike Smith <mike@virtualitconsulting.co.uk>
2022-09-28 09:55:31 +0100

    update location

--- rds.tf
+++ rds.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   db_name              = "mydb"
-  username             = "foo"
+  username             = "mikes"
   password             = "${file("../rds_pass.txt")}"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot = "true"
